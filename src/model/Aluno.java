66c48f26f81c0ba57a4e20fefe3e5e652f7d7c2e
nfandre <andrelive.05@hotmail.com>
2018-08-28 11:32:35 -0300

    commit inicial

---
+++ src/model/Aluno.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Aluno {
+	
+	private  String nome;
+	private String cpf;
+	private int email;
+	
+	public Aluno(String nome, String cpf, int email) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+}
