49463a16a8f4c100918712b6644b0517632b983c
nfandre <42738620+nfandre@users.noreply.github.com>
2018-08-28 21:35:50 -0300

    Add files via upload

---
+++ cadastroAluno/src/cadastroaluno/ProcessoMatricula.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastroaluno;
+
+/**
+ *
+ * @author André
+ */
+public class ProcessoMatricula {
+    Curso curso;
+    public void processoMatricula(Curso curso){
+        
+    }
+    public boolean verificarExistencia(String codigo){
+        return true;
+    }
+    private String gerarProntuario(){
+        return "kkkk";
+    }
+}
